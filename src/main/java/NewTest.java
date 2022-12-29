ef71af5ca307c7ef3cbe6648a7e36e85c90c1695
Adelina <adelina_usmanova@mail.ru>
2022-12-29 22:38:03 +0300

    new test added

---
+++ src/main/java/NewTest.java
@@ -0,0 +1,11 @@
+import com.sun.nio.sctp.Association;
+import org.junit.jupiter.api.Test;
+
+public class NewTest {
+
+    @Test
+    void newTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+
+}
